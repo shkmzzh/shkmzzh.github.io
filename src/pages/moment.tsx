d54efd9f79349e038b689818274e047891423cb4
shkmzzh <shkmzzh@users.noreply.github.com>
2026-04-08 10:12:52 +0800

    CI/CD和SSR修复

--- src/pages/moment.tsx
+++ src/pages/moment.tsx
@@ -2,7 +2,7 @@ import Layout from '@/components/Layout';
 import { ReactElement } from 'react';
 
 export default function Moment() {
-  return <div className={`flex`}>我叫牛马怀，是个老瓢虫🐞 </div>;
+  return <div className={`flex`}>我的龙虾是个蠢蛋🦞 </div>;
 }
 
 Moment.getLayout = function getLayout(page: ReactElement) {
